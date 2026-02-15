18c5f4d0695246efb3b44d3a74ee80a11b802108
gere29 <gerebenedict29@gmail.com>
2026-02-15 23:43:25 +0800

    Refactor GradeCalculator to GradeCalc with validation
    Updated the GradeCalculator class to GradeCalc and improved grade input validation.

--- ProgrammingParadigmsInJava/OOP.java
+++ ProgrammingParadigmsInJava/OOP.java
@@ -2,32 +2,44 @@ import java.util.Scanner;
 
 public class OOP {
     public static void main(String[] args) {
-        GradeCalculator calculator = new GradeCalculator();
+        GradeCalc calculator = new GradeCalc();
 
-        calculator.GradeInput();
-        double average = calculator.computeAverage();
+        calculator.inputGrades();
+        double average = calculator.computeAvg();
 
-        System.out.println("Average: " + average);
+        System.out.println("\nGrade Average: " + average);
     }
 }
 
 
-class GradeCalculator {
+
+
+class GradeCalc {
     private double[] grades = new double[5];
 
-    public void GradeInput() {
-        Scanner sc = new Scanner(System.in);
+    public void inputGrades() {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter 5 grades:");
+        System.out.println("Enter 5 grades:\n");
+
         for (int i = 0; i < 5; i++) {
-            System.out.print("Grade " + (i + 1) + ": ");
-            grades[i] = sc.nextDouble();
+            while (true) {
+                System.out.print("Grade number " + (i + 1) + ": ");
+                double input = scanner.nextDouble();
+
+                if (input >= 1 && input <= 100) {
+                    grades[i] = input;
+                    break;
+                } else {
+                    System.out.println("Invalid grade.");
+                }
+            }
         }
 
-        sc.close();
+        scanner.close();
     }
 
-    public double computeAverage() {
+    public double computeAvg() {
         double sum = 0;
         for (double grade : grades) {
             sum += grade;
