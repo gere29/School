1b98e5fced6566be8a5769398cd3e244c19a1eb0
gere29 <gerebenedict29@gmail.com>
2026-02-15 23:43:45 +0800

    Enhance grade input validation and user prompts
    Added input validation for grades to ensure they are between 1 and 100. Improved user prompts for clarity.

--- ProgrammingParadigmsInJava/Procedural.java
+++ ProgrammingParadigmsInJava/Procedural.java
@@ -3,20 +3,34 @@ import java.util.Scanner;
 public class Procedural {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         double sum = 0;
 
-        System.out.println("Enter 5 grades:");
+        System.out.println("Enter 5 grades:\n");
 
         for (int i = 1; i <= 5; i++) {
-            System.out.print("Grade " + i + ": ");
-            double grade = scanner.nextDouble();
-            sum += grade;
+
+            while (true) {
+                System.out.print("Grade number " + i + ": ");
+
+                if (scanner.hasNextDouble()) {
+                    double grade = scanner.nextDouble();
+
+                    if (grade >= 1 && grade <= 100) {
+                        sum += grade;
+                        break;
+                    } else {
+                        System.out.println("Invalid grade.");
+                    }
+
+                } else {
+                    System.out.println("Invalid input.");
+                    scanner.next();
+                }
+            }
         }
 
         double average = sum / 5;
-
-        System.out.println("Average: " + average);
+        System.out.println("\nGrade Average: " + average);
 
         scanner.close();
     }
