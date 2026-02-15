3598a4cf0aff0f8d5ac68e79a8ee61063fd90440
gere29 <gerebenedict29@gmail.com>
2026-02-15 23:55:11 +0800

    Refactor OOP class and rename GradeCalc to GradeCalculator

--- ProgrammingParadigmsInJava/OOP.java
+++ ProgrammingParadigmsInJava/OOP.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class OOP {
     public static void main(String[] args) {
-        GradeCalc calculator = new GradeCalc();
+        GradeCalculator calculator = new GradeCalculator();
 
         calculator.inputGrades();
-        double average = calculator.computeAvg();
+        double average = calculator.computeAverage();
 
         System.out.println("\nGrade Average: " + average);
     }
 }
 
-
-
-
-class GradeCalc {
+class GradeCalculator {
     private double[] grades = new double[5];
 
     public void inputGrades() {
@@ -23,15 +20,23 @@ class GradeCalc {
         System.out.println("Enter 5 grades:\n");
 
         for (int i = 0; i < 5; i++) {
-            while (true) {
-                System.out.print("Grade number " + (i + 1) + ": ");
-                double input = scanner.nextDouble();
 
-                if (input >= 1 && input <= 100) {
-                    grades[i] = input;
-                    break;
+            while (true) {
+                System.out.print("Grade " + (i + 1) + ": ");
+
+                if (scanner.hasNextDouble()) {
+                    double input = scanner.nextDouble();
+
+                    if (input >= 1 && input <= 100) {
+                        grades[i] = input;
+                        break;
+                    } else {
+                        System.out.println("Invalid grade.");
+                    }
+
                 } else {
-                    System.out.println("Invalid grade.");
+                    System.out.println("Invalid input.");
+                    scanner.next();
                 }
             }
         }
@@ -39,7 +44,7 @@ class GradeCalc {
         scanner.close();
     }
 
-    public double computeAvg() {
+    public double computeAverage() {
         double sum = 0;
         for (double grade : grades) {
             sum += grade;
