ec3e7f949d5cc0f652476544a337f6946a5fc080
gere29 <gerebenedict29@gmail.com>
2026-02-15 23:42:52 +0800

    Implement input validation for grade entries
    Added input validation for grades to ensure they are between 1 and 100.

--- ProgrammingParadigmsInJava/Functional.java
+++ ProgrammingParadigmsInJava/Functional.java
@@ -4,21 +4,35 @@ import java.util.Arrays;
 public class Functional {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         double[] grades = new double[5];
 
-        System.out.println("Enter 5 grades:");
+        System.out.println("Enter 5 grades:\n");
 
         for (int i = 0; i < 5; i++) {
-            System.out.print("Grade " + (i + 1) + ": ");
-            grades[i] = scanner.nextDouble();
+
+            while (true) {
+                System.out.print("Grade number " + (i + 1) + ": ");
+
+                if (scanner.hasNextDouble()) {
+                    double input = scanner.nextDouble();
+
+                    if (input >= 1 && input <= 100) {
+                        grades[i] = input;
+                        break;
+                    } else {
+                        System.out.println("Invalid grade.");
+                    }
+
+                } else {
+                    System.out.println("Invalid input.");
+                    scanner.next();
+                }
+            }
         }
 
-        double average = Arrays.stream(grades)
-                               .average()
-                               .orElse(0);
+        double average = Arrays.stream(grades).average().orElse(0);
 
-        System.out.println("Average: " + average);
+        System.out.println("\nGrade Average: " + average);
 
         scanner.close();
     }
